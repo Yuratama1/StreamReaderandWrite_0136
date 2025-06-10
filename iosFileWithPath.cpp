b079d27ae1cdb753b2369897a52e8d5cef9304ee
Yuratama1 <yuratama661@gmail.com>
2025-06-10 18:16:13 +0700

    menutup file dengan outfile.close

--- iosFileWithPath.cpp
+++ iosFileWithPath.cpp
@@ -23,4 +23,6 @@ int main(){
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalama menulis sekarang tutup filenya
+    outfile.close();
 }
