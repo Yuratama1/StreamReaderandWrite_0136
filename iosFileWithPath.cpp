3e95b8275c20b7b2d4586e83b907747c696d02c1
Yuratama1 <yuratama661@gmail.com>
2025-06-10 18:10:08 +0700

    membuat ios file dengan path

---
+++ iosFileWithPath.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namafile;
+    cout << "Masukkan nama file : ";
+    cin >> namafile;
+    
+}
