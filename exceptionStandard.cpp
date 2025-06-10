f567c0cddff4cf3620cf59eb9ebf902f6953ac1e
Yuratama1 <yuratama661@gmail.com>
2025-06-10 18:36:22 +0700

    menampilkan pesan array elemn 5 dan memanggil array elemen 5

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -7,5 +7,10 @@ using namespace std;
 
 int main(){
     cout << "Awal Program" << endl; // penanda 1:...
-    
+    try{
+        array<int, 3> data = { 10, 20, 30};
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+        //memangil array elemen 5
+    }
 }
