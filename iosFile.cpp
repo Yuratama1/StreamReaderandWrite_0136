cb379cace72b5c251203eb7ae4b400dc911405c2
Yuratama1 <yuratama661@gmail.com>
2025-06-10 17:33:45 +0700

    memebnarkan tanda ' ke tanda "

--- iosFile.cpp
+++ iosFile.cpp
@@ -17,7 +17,7 @@ int main(){
         //mendapatkan setiap karakter dalam satu baris
         getline(cin,baris);
         //loop akan berhenti jika anda memasukkan q
-        if(baris == 'q') break;
+        if(baris == "q") break;
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
